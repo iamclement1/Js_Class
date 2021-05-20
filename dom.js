3a6a9c2d583b475a93951231c9f1a5e914e386a8
iamclement1 <blessedmarcel1@gmail.com>
2021-05-20 16:37:03 +0100

    added form validation

--- dom.js
+++ dom.js
@@ -12,8 +12,43 @@
 // ul.lastElementChild.remove();
 // ul.firstElementChild.textContent = 'Hello';
 
-const btn = document.querySelector('.btn');
-btn.addEventListener('click', (e) => {
+// const btn = document.querySelector('.btn');
+// btn.addEventListener('click', (e) => {
+//   e.preventDefault();
+//   console.log('click');
+// });
+
+const myForm = document.querySelector('#my-form');
+const nameInput = document.querySelector('#name');
+const emailInput = document.querySelector('#email');
+const msg = document.querySelector('.msg');
+const userList = document.querySelector('#users');
+
+myForm.addEventListener('submit', onSubmit);
+
+function onSubmit(e) {
   e.preventDefault();
-  console.log('click');
-});
+
+  //   console.log(nameInput.value);
+
+  if (nameInput.value === '' || emailInput === '') {
+    msg.classList.add('error');
+    msg.innerHTML = 'Please enter fields';
+
+    // to make the error disapper
+    setTimeout(() => msg.remove(), 3000);
+  } else {
+    // console.log('success');
+
+    const li = document.createElement('li');
+    li.appendChild(
+      document.createTextNode(`${nameInput.value} : ${emailInput.value}`)
+    );
+
+    userList.appendChild(li);
+
+    //clear fields:
+   nameInput.value = '';
+   emailInput.value = '';
+  }
+}
